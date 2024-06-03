8f6225c1a968587ad6bddbd12bb3a07f9ebe96b5
Sai-Chakradhar <63908079+Sai-Chakradhar@users.noreply.github.com>
2024-06-03 16:48:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,4 @@
 import streamlit as st
-
-# Set the title of the app
-st.title("Addition of Two Numbers")
-
-# Get user input for the first number
-num1 = st.number_input("Enter the first number:", value=0)
-
-# Get user input for the second number
-num2 = st.number_input("Enter the second number:", value=0)
-
-# Add the two numbers
-result = num1 + num2
-
-# Display the result
-st.write("The result of the addition is:", result)
-st.write("hi",st.secrets["PROJECT_ID"])
-
-
-import streamlit as st
 import os
 import json
 
@@ -77,7 +58,7 @@ def multiturn_generate_content(text):
     )
     return x
 
-def app():
+def streamlit_app():
     # Title of the webpage
     st.title("Writer Project")
     
